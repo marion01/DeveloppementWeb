aee6a96a87174bbcb5e4910d2be2814e96c3d827
marion <marion_bertoni@outlook.fr>
2019-01-20 22:42:55 +0100

    ajout header access-control-allow-headers

--- myApp/app.js
+++ myApp/app.js
@@ -25,6 +25,8 @@ db.once("open", function() {
 
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+        
         next();
     });
   app.use(bodyParser.json());
